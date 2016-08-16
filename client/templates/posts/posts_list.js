93764a854b17f31b028ee58bf2fa0c60ac382ae0
iggelt <iggelt@ex.ua>
2016-08-16 09:19:18 +0300

    Update posts_list.js

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,9 @@
+Temlate.postsList.helpers({
+  postsWithRank: function(){
+    this.posts.rewind();
+    return this.posts.map(function(post,index,cursor){
+      post._rank = index;
+      return post;
+    });
+  }
+});
